54aa49847c2fe88eca8a8408dc6d2e1f66b48eca
Calum Halcrow <calumhalcrow@gmail.com>
2011-07-31 02:44:53 +0800

    Handling of static URLs.

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,6 +13,9 @@ urlpatterns = patterns('',
     url(r'^events/', include('frontend.events.urls')),
     url(r'', include('frontend.app.urls')),
     url(r'', include('social_auth.urls')),
+
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
 
     # Examples:
     # url(r'^$', 'frontend.views.home', name='home'),
