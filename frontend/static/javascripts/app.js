1c1236c78552a411ee82327313c4c6ad7ee81816
Calum Halcrow <calumhalcrow@gmail.com>
2011-07-31 06:51:33 +0800

    Shells of thumbs-up and status update JS.

--- frontend/static/javascripts/app.js
+++ frontend/static/javascripts/app.js
@@ -6,8 +6,7 @@ $(function() {
             form.attr('action'),
             form.serialize(),
             function(response) {
-                if (response.success === 'success')
-                {
+                if (response.success === 'success') {
                     $('speech', '.my-status').html(status_message);
                 }
             },
@@ -15,4 +14,40 @@ $(function() {
         );
         return false;
     });
+
+    $('.vote').click(function() {
+        var link = $(this);
+        var url_components = link.attr('href').split('?');
+        $.post(
+            url_components[0],
+            url_components[1],
+            function(response) {
+                if (response.status === 'success') {
+                    link.hide();
+                    link.siblings('.voted').show();
+                }
+            },
+            'json'
+        );
+        return 'false';
+    });
+
+
+    setTimeout("update_participants()", 3000);
 });
+
+
+function update_participants() {
+    var event_id = '1001';
+    $.get(
+        '/api/event/'+event_id+'/messages',
+        function(response) {
+            if (typeof response.statuses == 'Object') {
+                for (statu in response.statuses) {
+                    $('#' + statu.user.username + ' .message').html(statu.message);
+                }
+            }
+        },
+        'json',
+    );
+}
