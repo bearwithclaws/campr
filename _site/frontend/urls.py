790fd65edb833e1207fa48e3b00d106311569d10
bearwithclaws <foress@gmail.com>
2011-07-31 06:28:58 +0800

    added thumbs up

---
+++ _site/frontend/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 'frontend.app.views.index', name='home'),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('frontend.app.urls')),
+    url(r'', include('social_auth.urls')),
+
+    # Examples:
+    # url(r'^$', 'frontend.views.home', name='home'),
+    # url(r'^frontend/', include('frontend.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+)
