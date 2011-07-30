eccaa6e13e8264b061d9245304f8be6d1c26d781
felix <felix@felix-eeepc.(none)>
2011-07-31 05:59:29 +0800

    Implemented API call.

---
+++ frontend/api/handlers.py
@@ -0,0 +1,46 @@
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from django.db.models import Max, F
+from piston.handler import BaseHandler
+from piston.utils import rc, require_mime, require_extended
+from piston.utils import validate
+
+from frontend.events.models import Event, Status
+
+class EventHandler(BaseHandler):
+    model = Event
+
+    def read(self, request, *args, **kwargs):
+        qs = self.queryset(request)
+        if args != () or kwargs != {}:
+            try:
+                return qs.get(*args, **kwargs)
+            except ObjectDoesNotExist:
+                return rc.NOT_FOUND
+            except MultipleObjectsReturned: # should never happen, since we're using a PK
+                return rc.BAD_REQUEST
+        else:
+            return qs
+
+class StatusHandler(BaseHandler):
+    model = Status
+    fields = ('message', 'time', ('user', ('username', 'id')))
+
+    def read(self, request, *args, **kwargs):
+        qs = self.queryset(request)
+        return qs.filter(*args, **kwargs) \
+                .annotate(last_update=Max('user__status__time')) \
+                .filter(time=F('last_update'))
+
+    def create(self, request):
+        if not hasattr(request, "data"):
+            request.data = request.POST
+
+        attrs = self.flatten_dict(request.data)
+        if self.exists(**attrs):
+            return rc.DUPLICATE_ENTRY
+        else:
+            status = Status(event=attrs['event'],
+                        user=request.user,
+                        message=attrs['message'])
+            status.save()
+            return status
---
+++ frontend/api/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls.defaults import patterns, include, url
+from piston.resource import Resource
+from frontend.api.authentication import DjangoAuthentication
+from frontend.api.handlers import EventHandler, StatusHandler
+
+django_auth = DjangoAuthentication()
+
+events = Resource(handler=EventHandler, authentication=django_auth)
+statuses = Resource(handler=StatusHandler, authentication=django_auth)
+
+urlpatterns = patterns('frontend.api.views',
+    url(r'^events/(?P<id>\d+)/$', events),
+    url(r'^events/(?P<event>\d+)/statuses/$', statuses),
+)
