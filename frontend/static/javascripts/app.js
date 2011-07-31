17f911783146475bd1014a75360b36cfb80be78c
Calum Halcrow <calumhalcrow@gmail.com>
2011-07-31 09:34:01 +0800

    "working" status update.

--- frontend/static/javascripts/app.js
+++ frontend/static/javascripts/app.js
@@ -2,17 +2,19 @@ $(function() {
     $('#update-status').submit(function() {
         var form = $(this);
         var status_message = form.find('textarea[name=status]').val();
-        $.post(
-            form.attr('action'),
-            form.serialize(),
-            function(response) {
-                if (response.success === 'success') {
-                    $('speech', '.my-status').html(status_message);
-                }
-            },
-            'json'
-        );
+        $('.speech', '.my-status').html(status_message);
         return false;
+        // $.post(
+        //     form.attr('action'),
+        //     form.serialize(),
+        //     function(response) {
+        //         if (response.success === 'success') {
+        //             $('speech', '.my-status').html(status_message);
+        //         }
+        //     },
+        //     'json'
+        // );
+        // return false;
     });
 
     // $('.vote').click(function() {
