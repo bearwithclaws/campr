3539b48e0ba3329fda57d3650f16d5f85a7475a9
Calum Halcrow <calumhalcrow@gmail.com>
2011-08-02 21:13:17 +0800

    Pass event model instance to templates.
    So that we can display event details etc.

--- frontend/events/views.py
+++ frontend/events/views.py
@@ -2,17 +2,20 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from social_auth import __version__ as version
+from frontend.events.models import Event
 
 # Create your views here.
 @login_required
 def dashboard(request, event_id):
     """Login complete view, displays user data"""
-    event_id = '101'
+
+    event = Event.objects.get(id=event_id)
+
     ctx = {'accounts': request.user.social_auth.all(),
            'version': version,
            'last_login': request.session.get('social_auth_last_login_backend'),
            'username': request.user.username,
-            'update_status_action': '/api/event/'+event_id+'/status',
+           'event': event,
            }
     return render_to_response('events/dashboard.html', ctx, RequestContext(request))
 
