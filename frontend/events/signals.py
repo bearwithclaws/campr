f7bde18df9912ee86820e1ea78047e0cbb7b06b7
Seh Hui, Leong <felixleong@gmail.com>
2011-08-23 04:40:49 +0800

    Have Django signal our RabbitMQ when status are added.

--- frontend/events/signals.py
+++ frontend/events/signals.py
@@ -1,12 +1,20 @@
 from django.conf import settings
+from django.core import serializers
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import pika
 from events.models import Status, Vote
 
 @receiver(post_save, sender=Status)
 def on_status_received(sender, instance, created, **kwargs):
-    print ' [x] Status received - {0}: {1}'.format(
-            settings.RABBITMQ_QUEUE_NAME, instance.message)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(**settings.RABBITMQ_CONN))
+    channel = connection.channel()
+    channel.queue_declare(queue=settings.RABBITMQ_QUEUE_NAME)
+
+    data = serializers.serialize('json', [instance])
+    channel.basic_publish(exchange='', routing_key='hello', body=data)
+    print ' [x] Data sent to queue: {0}'.format(data)
+    connection.close()
 
 @receiver(post_save, sender=Vote)
 def on_vote_received(sender, instance, created, **kwargs):
