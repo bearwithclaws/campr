91953aa004fc9ef9c2cb65a3d04aec74a1a68b6b
Calum Halcrow <calumhalcrow@gmail.com>
2011-09-20 15:40:11 +0800

    Lists current events on homepage.
    Maybe we can do this better later, but for now it adds a nice
    continuity to the app in general: no need to know which URL to
    type into the browser location bar once you've created an event
    in the Admin section.

--- frontend/app/views.py
+++ frontend/app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from frontend.events.models import Event
 
 def index(request):
-    return render_to_response('app/index.html', context_instance=RequestContext(request))
+    ctx = {
+        'events': Event.objects.all(),
+    }
+    return render_to_response('app/index.html', ctx, RequestContext(request))
 
