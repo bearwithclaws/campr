d568681fb6c008e52727339f93207c7ba193ce6f
Calum Halcrow <calumhalcrow@gmail.com>
2011-10-12 21:10:23 +0800

    Integrates design v.2

--- frontend/media/javascripts/app.js
+++ frontend/media/javascripts/app.js
@@ -1,67 +1,41 @@
-$(function() {
+/* 
+* Skeleton V1.0.2
+* Copyright 2011, Dave Gamache
+* www.getskeleton.com
+* Free to use under the MIT license.
+* http://www.opensource.org/licenses/mit-license.php
+* 5/20/2011
+*/	
+	
 
-    var status_bubble = $('.my-status .bubble');
-    var status_text = status_bubble.find('.speech');
-    var update_status = $('#update-status');
+$(document).ready(function() {
 
-    if (status_text.html().length === 0) {
-        status_bubble.hide();
-    }
-
-    update_status.find('textarea[name=message]').focus(function() {
-        $(this).val("");
-    }).blur(function() {
-        if ($(this).val() === "") {
-            $(this).val(status_text.html());
-        }
-    });
-
-    $('#update-status').submit(function() {
-        var form = $(this);
-        var status_message = form.find('textarea[name=message]').val();
-        $.post(
-            form.attr('action'),
-            form.serialize(),
-            function(response) {
-                if (response.message) {
-                    status_bubble.show().find('.speech').html(response.message);
-                }
-            },
-            'json'
-        );
-        return false;
-    });
-
-    // $('.vote').click(function() {
-    //     var link = $(this);
-    //     var url_components = link.attr('href').split('?');
-    //     $.post(
-    //         url_components[0],
-    //         url_components[1],
-    //         function(response) {
-    //             if (response.status === 'success') {
-    //                 link.hide();
-    //                 link.siblings('.voted').show();
-    //             }
-    //         },
-    //         'json'
-    //     );
-    //     return 'false';
-    // });
-});
-
-$(function() {
-    var s = new io.Socket(window.location.hostname, {port: 8001, rememberTransport: false});
-    s.connect();
-
-    s.addEvent('message', function(data) {
-        messages = JSON.parse(data);
-
-        // why doesn't for..in work here?
-        for (var i=0; i < messages.length; i++) {
-            message = messages[i];
-            speech = $('#'+message.fields.checkin+' div.speech');
-            speech.hide().html(message.fields.message).fadeIn('slow');
-        }
-    });
+	/* Tabs Activiation
+	================================================== */
+	var tabs = $('ul.tabs');
+	
+	tabs.each(function(i) {
+		//Get all tabs
+		var tab = $(this).find('> li > a');
+		tab.click(function(e) {
+			
+			//Get Location of tab's content
+			var contentLocation = $(this).attr('href') + "Tab";
+			
+			//Let go if not a hashed one
+			if(contentLocation.charAt(0)=="#") {
+			
+				e.preventDefault();
+			
+				//Make Tab Active
+				tab.removeClass('active');
+				$(this).addClass('active');
+				
+				//Show Tab Content & add active class
+				$(contentLocation).show().addClass('active').siblings().hide().removeClass('active');
+				
+			} 
+		});
+	}); 
+	
 });
