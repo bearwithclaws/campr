4d817324bda1de7f3b55c9b0f005614a28e87419
Seh Hui Leong <felixleong@gmail.com>
2011-10-14 11:25:21 +0800

    Support URL structures based on slugs.

--- frontend/events/views.py
+++ frontend/events/views.py
@@ -5,13 +5,16 @@ from frontend.events.models import Event, Checkin
 from django.contrib.auth.models import User
 
 # Create your views here.
-def dashboard(request, event_id):
+def dashboard(request, event_id=None, slug=''):
     """Login complete view, displays user data"""
 
-    event = get_object_or_404(Event, id=event_id)
+    if(event_id is not None):
+        event = get_object_or_404(Event, id=event_id)
+    else:
+        event = get_object_or_404(Event, slug=slug)
     user = request.user
-    checkin = Checkin.objects.get(event=event.id, user=user.id)
-    checkins = Checkin.objects.filter(event=event.id)
+#    checkin = Checkin.objects.get(event=event.id, user=user.id)
+#    checkins = Checkin.objects.filter(event=event.id)
 
     #TODO: Get list of users who are checked in the same event and their latest
     #      status
@@ -21,16 +24,19 @@ def dashboard(request, event_id):
 
     ctx = {
         'event': event,
-        'checkin': checkin,
-        'checkins': checkins,
+#        'checkin': checkin,
+#        'checkins': checkins,
         'last_login': request.session.get('social_auth_last_login_backend'),
     }
     return render_to_response('events/dashboard_loggedin.html', ctx, RequestContext(request))
 
 @login_required
-def checkin(request, event_id):
+def checkin(request, event_id, slug=''):
     """Checks-in to the event"""
-    event = get_object_or_404(Event, id=event_id)
+    if(event_id is not None):
+        event = get_object_or_404(Event, id=event_id)
+    else:
+        event = get_object_or_404(Event, slug=slug)
     user = request.user
 
     checkin, created = Checkin.objects.get_or_create(event=event, user=user)
