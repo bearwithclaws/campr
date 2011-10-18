9f43d7acfee04ee8119b6052186019a93ac1af66
Calum Halcrow <calumhalcrow@gmail.com>
2011-10-18 18:48:04 +0800

    Throw Checkin info onto bus post_save also.

--- frontend/events/signals.py
+++ frontend/events/signals.py
@@ -3,16 +3,31 @@ from django.core import serializers
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 import pika
-from events.models import Message
+from events.models import Message, Checkin
+from django.utils import simplejson
 
-@receiver(post_save, sender=Message)
-def on_status_received(sender, instance, created, **kwargs):
+def publish(data):
     connection = pika.BlockingConnection(pika.ConnectionParameters(**settings.RABBITMQ_CONN))
     channel = connection.channel()
     channel.queue_declare(queue=settings.RABBITMQ_QUEUE_NAME)
 
-    data = serializers.serialize('json', [instance])
     channel.basic_publish(exchange='', routing_key='hello', body=data)
     print ' [x] Data sent to queue: {0}'.format(data)
     connection.close()
 
+@receiver(post_save, sender=Message)
+def on_message_received(sender, instance, created, **kwargs):
+    publish(serializers.serialize('json', [instance]))
+
+@receiver(post_save, sender=Checkin)
+def on_checkin_received(sender, instance, created, **kwargs):
+    checkin_info = {
+        'checkin': {
+            'id':                instance.id,
+            'username':          instance.user.username,
+            'profile_image_url': instance.profile_image_url(),
+            'present':           instance.present,
+            'latest_message':    instance.latest_message(),
+        },
+    }
+    publish(simplejson.dumps(checkin_info))
