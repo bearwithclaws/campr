959f07cb131badfecf91b5f981d6823a951515e3
Calum Halcrow <calumhalcrow@gmail.com>
2012-02-13 19:06:32 +0800

    Removes RabbitMQ-related code.
    And also chatroom.py, which has sort-of become campr.py.

--- frontend/events/models.py
+++ frontend/events/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 import tweepy
-try:
-    from events.signals import *
-except:
-    pass
-
 
 class Event(models.Model):
     name = models.CharField(max_length=100)
