4bcf03db3d866b35c910e297adbaf7b4d28a657d
Calum Halcrow <calumhalcrow@gmail.com>
2012-07-22 13:56:55 +0800

    Removes AJAX API. We'll use the socket.

--- frontend/urls.py
+++ frontend/urls.py
@@ -11,7 +11,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^accounts/', include('frontend.accounts.urls')),
-    url(r'^api/', include('frontend.api.urls')),
     url(r'^events/', include('frontend.events.urls')),
     url(r'', include('social_auth.urls')),
 
