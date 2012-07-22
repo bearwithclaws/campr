bae0485495aaa2be173cdf1e242e81e98b86d32e
Calum Halcrow <calumhalcrow@gmail.com>
2012-07-22 15:47:10 +0800

    Revert "Removes AJAX API. We'll use the socket."
    This reverts commit 4bcf03db3d866b35c910e297adbaf7b4d28a657d.

---
+++ frontend/api/handlers.py
@@ -0,0 +1,65 @@
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from django.db.models import Max, F
+from piston.handler import BaseHandler
+from piston.utils import rc
+
+from frontend.events.models import Event, Message, Vote, Checkin
+
+class EventHandler(BaseHandler):
+    model = Event
+
+    def read(self, request, *args, **kwargs):
+        qs = self.queryset(request)
+        if args != () or kwargs != {}:
+            try:
+                return qs.get(*args, **kwargs)
+            except ObjectDoesNotExist:
+                return rc.NOT_FOUND
+            except MultipleObjectsReturned: # should never happen, since we're using a PK
+                return rc.BAD_REQUEST
+        else:
+            return qs
+
+class MessageHandler(BaseHandler):
+    model = Message
+    fields = ('message', 'time', ('checkin', ('user_id', )), )
+
+    def read(self, request, *args, **kwargs):
+        qs = self.queryset(request)
+
+        if 'event_id' not in kwargs:
+            return rc.NOT_FOUND
+        event_id = kwargs['event_id']
+
+        return qs.filter(checkin__event=event_id) \
+                .annotate(last_update=Max('checkin__message__time')) \
+                .filter(time=F('last_update'))
+
+    def create(self, request, event_id):
+        if not hasattr(request, "data"):
+            request.data = request.POST
+
+        attrs = self.flatten_dict(request.data)
+        message = Message(checkin=Checkin.objects.get(user=request.user.id, event=event_id),
+                          message=attrs['message'])
+        message.save()
+        return message
+
+class VoteHandler(BaseHandler):
+    model = Vote
+
+    def read(self, request, *args, **kwargs):
+        qs = self.queryset(request)
+        return qs.filter(*args, **kwargs)
+
+    def create(self, request):
+        if not hasattr(request, "data"):
+            request.data = request.POST
+
+        attrs = self.flatten_dict(request.data)
+
+        vote = Vote(event=attrs['event'],
+                    voter=request.user,
+                    recipient=attrs['recipient'])
+        vote.save()
+        return vote
--- frontend/urls.py
+++ frontend/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^accounts/', include('frontend.accounts.urls')),
+    url(r'^api/', include('frontend.api.urls')),
     url(r'^events/', include('frontend.events.urls')),
     url(r'', include('social_auth.urls')),
 
